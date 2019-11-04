1d0467533c09bda6abeb7dd8d3322f2a26e18d58
pdsva-iscteiul <pdsva@iscte-iul.pt>
2019-11-04 15:10:53 +0000

    3rd commit

--- ProjectEngSoft/src/test.java
+++ ProjectEngSoft/src/test.java
@@ -1,9 +1,9 @@
-
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method st
+		
+		System.out.println("2nd");
 	}
 
 }
