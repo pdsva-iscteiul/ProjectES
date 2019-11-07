b4321a39a893bee336752afd91a10595395496b3
Filipe <Filipe@192.168.1.67>
2019-11-07 19:18:09 +0000

    Menu gui, early version

--- ProjectEngSoft/src/GUI/GUImenu.java
+++ ProjectEngSoft/src/GUI/GUImenu.java
@@ -3,6 +3,13 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUImenu {
 
@@ -36,8 +43,34 @@ public class GUImenu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 705, 551);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Rule maker");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(119, 166, 454, 89);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnEvaluateRule = new JButton("Evaluate Rule");
+		btnEvaluateRule.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnEvaluateRule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnEvaluateRule.setBounds(119, 266, 454, 89);
+		frame.getContentPane().add(btnEvaluateRule);
+		
+		JButton btnSeeRules = new JButton("See rules");
+		btnSeeRules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSeeRules.setBounds(119, 366, 454, 89);
+		frame.getContentPane().add(btnSeeRules);
+		
+		JLabel lblMenu = new JLabel("MENU");
+		lblMenu.setForeground(Color.BLACK);
+		lblMenu.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMenu.setFont(new Font("Lucida Console", Font.PLAIN, 35));
+		lblMenu.setBounds(45, 11, 615, 127);
+		frame.getContentPane().add(lblMenu);
 	}
-
 }
