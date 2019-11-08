a9f3b38f4195b7ed3c55388511b4f6bf139ad319
sadfo-iscteiul <sadfo@iscte-iul.pt>
2019-11-08 01:03:23 +0000

    Make the GUI

--- ProjectEngSoft/src/GUI/GUIexcelPage.java
+++ ProjectEngSoft/src/GUI/GUIexcelPage.java
@@ -26,7 +26,7 @@ public class GUIexcelPage {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void newWindow() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
